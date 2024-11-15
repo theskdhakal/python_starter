3533a06d854e549a7c8793343d43045d0ff6f395
Shiva Dhakal <theskdhakal@gmail.com>
2024-11-15 16:15:38 +1100

    day 1 completed

---
+++ day-1/main.py
@@ -0,0 +1,41 @@
+
+# (1) 
+# print('I will be one year old on ' + input('whats your DOB'))
+
+#input takes the input first from user
+#convert the input as statement and print as a whole
+
+
+# (2)
+# name=input("Write your Name ")
+# print(len(name))
+
+
+#(3)
+# a=input("a:")
+# b=input("b:")
+
+# x=a
+# b=x
+# a=b
+
+# print ("a = " + a)
+# print ("b = " + b)
+
+
+
+
+# ***************************** BAND NAME GENERATOR *******************************************************
+
+#  create a greeting for your program
+print( 'Welcome to Band-Name generator program' )
+
+# Ask user for the name of city that they grew up in 
+city= input( 'whats name of city you grew up in?' '\n')
+
+# Ask user for name of a pet 
+pet=input ('write name of a pet' '\n')
+
+
+#combine the name of city and pet and show them their band name
+print("the name of your band is : " + city +" " + pet)
