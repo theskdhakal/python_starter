9e0efe33ccc394183cd8fcce51966b8573f8ff4b
Shiva Dhakal <theskdhakal@gmail.com>
2024-11-17 11:00:30 +1100

    day2 first challenge done

---
+++ day-2/main2.py
@@ -0,0 +1,31 @@
+# Data types 
+
+# String
+
+"Hello"
+
+print("124"+"432")
+
+#integer
+
+print(123 + 456)
+
+#float -- floating point number ( the one with decimal)
+
+1.43476
+
+#boolean
+True
+False
+
+
+
+
+# ****************************challenge DAY 2 ********************************
+
+three_digit_number=input("Enter a three digit number: ")
+print(three_digit_number)
+
+
+print(int(three_digit_number[1]) + int(three_digit_number[2]) + int(three_digit_number[0]))
+
