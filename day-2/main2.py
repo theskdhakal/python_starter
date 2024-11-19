065c358505150a3b05b007170f4b8286d775fdf8
Shiva Dhakal <theskdhakal@gmail.com>
2024-11-19 11:21:36 +1100

    bmi calculator challenge completed

--- day-2/main2.py
+++ day-2/main2.py
@@ -29,3 +29,19 @@ print(three_digit_number)
 
 print(int(three_digit_number[1]) + int(three_digit_number[2]) + int(three_digit_number[0]))
 
+
+# Mathematical operation
+# ** ----- gives exponention value
+# 2 ***3 it means 2 power 3 i.e 2*2*2 
+
+print (3+3*3/3-3)
+
+
+# ************************ BMI CALCULATOR ***********************************************
+height=float(input('enter your height in m: '))
+weight=float(input('enter your weight in kg: '))
+
+
+BMI= (weight)/(height * height)
+
+print(int(BMI))
