22a1b5ae04f47a4d5f012817d023b794ea47c101
Shiva Dhakal <theskdhakal@gmail.com>
2024-11-20 21:54:41 +1100

    pizza-order challenge done

---
+++ day-3/pizza-order.py
@@ -0,0 +1,32 @@
+print("Welcome to Python Pizza deliveries !")
+
+size=input("What size pizzqa do you want ? S , M , or L ")
+
+print(size)
+
+if size=="S":
+    price=15
+    print(f"Price of pizza is ${price}")
+if size=="M":
+     price=20
+     print(f"Price of pizza is ${price}")
+if size=="L":
+      price=25
+      print(f"Price of pizza is ${price}")
+
+pepporini=input("Do you want pepporoni ? Y or N ")
+if pepporini=="Y" :
+    if size=="S":
+         price += 2
+    else:
+         price +=3
+
+cheese=input("Do you want cheese in your pizza ? Y or N ")
+if cheese=="Y":
+     price +=1
+
+print(f"Total price of your pizza is {price}")
+
+
+
+ 
