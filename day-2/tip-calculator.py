75503972e28125e47a94005a598cbffc6937ce5c
Shiva Dhakal <theskdhakal@gmail.com>
2024-11-20 11:22:28 +1100

    tip calculator built using python

---
+++ day-2/tip-calculator.py
@@ -0,0 +1,17 @@
+print("Welcome to the tip calculator.")
+bill=int(input("What was the total bill ? "))
+print(bill)
+
+tip=int(input("What percentage tip would you like to give ? "))
+# print(str(tip) + "%")
+print(f"{tip} % ")
+
+
+
+people=int(input("How many people to split the bill ? "))
+print(people)
+
+total_amount= bill + ((tip/100)*bill)
+your_share=round(total_amount/people,2)
+
+print(f"Each person should pay: {your_share}")
