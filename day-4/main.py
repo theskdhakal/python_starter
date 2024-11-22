a98dd503a6074ee7b71d3d8ccaeb59072446bbb2
Shiva Dhakal <theskdhakal@gmail.com>
2024-11-22 16:52:55 +1100

    coin toss challenge completed using randomisation module

---
+++ day-4/main.py
@@ -0,0 +1,24 @@
+import random
+import my_module
+
+
+
+
+# way to generate the random integer in pyhton
+
+random_integer= random.randint(1,100)
+print(random_integer)
+
+# Module
+print(my_module.pi)
+
+
+# generate random floating points
+random_floating=random.random() 
+# the code above genrate random float number between 0 and 1
+
+random_floating_between=random.uniform(1.5,7.8)
+print(random_floating_between)
+
+print(random_floating)
+
