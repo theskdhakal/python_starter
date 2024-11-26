748ad3230cfffa2a1d208e18717589a977bfd898
Shiva Dhakal <theskdhakal@gmail.com>
2024-11-26 13:08:54 +1100

    working on pwd generator

---
+++ day-5/PyPassword-generator.py
@@ -0,0 +1,31 @@
+import random;
+
+letters=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']   
+
+numbers=["1","2","3","4","5","6","7","8","9","0"]
+
+symbols=["!","@","#","$","%","^","&","*"]
+
+
+print("Welcom to pypassword generator")
+
+nr_letters=int(input("How many letters would you like in your password? \n"))
+nr_symbols=int(input("How many symbols would you like in your password? \n"))
+nr_numbers=int(input("How many numbers would you like in your password? \n"))
+
+# //choosing the multiple items from array  depending upon number of required items
+random_letters=random.sample(letters,nr_letters)
+random_symbols=random.sample(symbols,nr_numbers)
+random_numbers=random.sample(numbers,nr_symbols)
+
+random_password_list=random_letters + random_symbols + random_numbers
+
+print(random_password_list)
+
+# shuffle the items in array
+shuffled_passowords_list=random.shuffle(random_password_list)
+print(shuffled_passowords_list)
+
+# random_password=''.join(shuffled_passowords_list)
+# print(random_password)
+
