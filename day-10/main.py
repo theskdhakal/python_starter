093765717ed174cc6db9680f6dbb634b1786bb6f
Shiva Dhakal <theskdhakal@gmail.com>
2024-12-07 13:37:11 +1100

    calculating days in month using function output

---
+++ day-10/main.py
@@ -0,0 +1,12 @@
+#functions with output
+
+
+def format_name(f_name,l_name):
+    
+    name=f_name.title() + l_name.title()
+   
+    return name
+
+
+final_name=format_name("shiva","dhakal")
+print(final_name)
