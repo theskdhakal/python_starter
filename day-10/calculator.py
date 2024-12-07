da62114018ccbdce3382334fffe746296c3742c3
Shiva Dhakal <theskdhakal@gmail.com>
2024-12-08 10:37:35 +1100

    working on calculator using dictionary

---
+++ day-10/calculator.py
@@ -0,0 +1,38 @@
+#calculator
+
+#add
+def add(n1,n2):
+    return n1+n2
+
+#subtract
+def subtract(n1,n2):
+    return n1-n2
+
+#add
+def multiply(n1,n2):
+    return n1*n2
+
+#add
+def division(n1,n2):
+    return n1/n2
+
+
+{
+    "+":add,
+    "-":subtract,
+    "*":multiply,
+    "/":division
+}
+
+
+# operations=[]
+
+# def calculation(key,value):
+#     new_operation={
+#         key:value
+#     }
+
+#     operations.append(new_operation)
+#     print(operations)
+
+# calculation("add","+")
--- day-10/main.py
+++ day-10/main.py
@@ -1,7 +1,9 @@
 #functions with output
 
-
+# //single return value in function
 def format_name(f_name,l_name):
+    """ Take a first and last name and
+      format it to return thr title case version of name"""
     
     name=f_name.title() + l_name.title()
    
@@ -10,3 +12,9 @@ def format_name(f_name,l_name):
 
 final_name=format_name("shiva","dhakal")
 print(final_name)
+
+
+
+
+# //mutiple return value in function 
+# mutiple return can be used in a function using if condition
