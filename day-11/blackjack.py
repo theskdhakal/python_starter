4802b01ec2cb1342744dea82b554ebfb18fed532
Shiva Dhakal <theskdhakal@gmail.com>
2024-12-16 08:59:59 +1100

    function to calculate score in blackjack

--- day-11/blackjack.py
+++ day-11/blackjack.py
@@ -1,4 +1,5 @@
 from art import logo
+import random
 
 print(logo)
 
@@ -6,6 +7,20 @@ print(logo)
 
 
 print("WELCOME TO BLACKJACK")
+ 
+
+cards=[11,2,3,4,5,6,7,8,9,10,10,10,10]
+
+computer=random.sample(cards,2)
+print(computer)
+
+
+human=random.sample(cards,2)
+print(human)
+
+
+
+
 
 
 
