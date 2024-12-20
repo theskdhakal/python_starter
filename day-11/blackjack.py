56e09727b8e2ce0cd1a37983924f3030e48f7452
Shiva Dhakal <theskdhakal@gmail.com>
2024-12-20 22:01:58 +1100

    if condition written for game over

--- day-11/blackjack.py
+++ day-11/blackjack.py
@@ -21,75 +21,10 @@ def deal_card():
      return card
 
 
-user_cards=[]
-computer_cards=[]
-
-for _ in range(2):
-     user_cards.append(deal_card())
-     computer_cards.append(deal_card())
-
-# computer_card=random.sample(cards,2)
-# print(computer_card)
-
-
-# human_card=random.sample(cards,2)
-# print(human_card)
-
-
-# def add_score():
-#     human_score=sum(human_card)
-#     computer_score=sum(computer_card)
-
-   
-    
-#     if (human_score==21):
-#          print("Blackjack:You won the game")
-#          return 0
-#     elif (computer_score==21):
-#          print("Blackjack:Computer won the game")
-#          return 0
-#     elif human_score >21:
-#          if 11 in human_card:
-#               human_card.remove(11)
-#               human_card.append(1)
-
-#               if human_score >21:
-#                    print("You lose")
-              
-#          else:
-#               print("You lose")
-     
-
-# def calculate_score():
-
-#          add_score()
-
-#          another=input("Do you want to draw another card ? Y or N : ").lower()
-#          print(another)
-
-#          if another == "y":
-#                human_another=random.choice(cards)
-#                human_card.append(human_another)
-
-#                add_score()
-#          else:
-#                if computer_score < 17:
-#                      computer_another=random.choice(cards)
-
-#                      computer_card.append(computer_another)
-#                      add_score()
-
-#                      if computer_score == 21 or computer_score > human_score:
-#                            print("computer won")
-#                      else:
-#                            print("You won")
-               
-# calculate_score()
-
-
 
 def calculate_score(cards):
-     cards=[1,5,3,4]
+     """ Take a list of card and return the sum of the card"""
+     
      # sum use take a iterable list as argument and perform additon from left to right
 
      if sum(cards)==21 and len(cards) ==2:
@@ -101,3 +36,23 @@ def calculate_score(cards):
           cards.append(1)
      return sum(cards)  
 
+
+
+user_cards=[]
+computer_cards=[]
+is_game_over=False
+
+for _ in range(2):
+     user_cards.append(deal_card())
+     computer_cards.append(deal_card())
+
+
+user_score=calculate_score(user_cards)
+computer_score=calculate_score(computer_cards)
+
+print(f"Your cards: {user_cards}, current score:{user_score}")
+print(f"computer's first card: {computer_cards[0]}")
+
+
+if user_score==0 or computer_score==0 or user_score > 21:
+     is_game_over=True
