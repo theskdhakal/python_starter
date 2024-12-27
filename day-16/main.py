85bdafc2906b12c5fc1da1589681528dd5a3863d
Shiva Dhakal <theskdhakal@gmail.com>
2024-12-28 10:13:48 +1100

    modifying object Attributes and Calling Methods

--- day-16/main.py
+++ day-16/main.py
@@ -1,31 +1,41 @@
-
-import another_module
-print(another_module.another_variable)
-
-# in above case, we have tapped inside a variable called another_variable located in another_module
-
-
-#  in the case below, we have tapped inside the Turtle class( class has capital case) inside the turtle module
-
-# import turtle
 #
-# timmy=turtle.Turtle()
+# import another_module
+# print(another_module.another_variable)
+#
+# # in above case, we have tapped inside a variable called another_variable located in another_module
+#
+#
+# #  in the case below, we have tapped inside the Turtle class( class has capital case) inside the turtle module
+#
+# # import turtle
+# #
+# # timmy=turtle.Turtle()
+#
+# #  insted of above, we can import Turtle class from turtle module and just call it
+#
+# from turtle import Turtle, Screen
+#
+# timmy=Turtle()
+# timmy.shape("turtle")
+# timmy.color("red")
+# timmy.forward(100)
+# # timmy is a new object
+#
+#
+# # ____________________________________object attribute___________________________________
+# my_screen=Screen()
+# print(my_screen.canvheight)
+# #  my_screen here is object and canvaheight i s the attribute
+#
+# # _____________________________________object method__________________________________________
+# my_screen.exitonclick()
 
-#  insted of above, we can import Turtle class from turtle module and just call it
-
-from turtle import Turtle, Screen
-
-timmy=Turtle()
-timmy.shape("turtle")
-timmy.color("red")
-timmy.forward(100)
-# timmy is a new object
 
 
-# ____________________________________object attribute___________________________________
-my_screen=Screen()
-print(my_screen.canvheight)
-#  my_screen here is object and canvaheight i s the attribute
-
-# _____________________________________object method__________________________________________
-my_screen.exitonclick()
+from prettytable import PrettyTable
+table=PrettyTable()
+table.add_column(" Pokemon Name ",["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type",["Electric","Water","Fire"])
+table.align='l'
+table.border=False
+print(table)
