7e546fdbbef6da6a9a403ae0ec64dfcf918c98c2
Shiva Dhakal <theskdhakal@gmail.com>
2024-12-30 11:19:35 +1100

    quiz game was completed using OOP

--- day-17-Constructing-Class/main.py
+++ day-17-Constructing-Class/main.py
@@ -12,8 +12,13 @@ for item in question_data:
     question_bank.append(question_item)
 
 quiz=QuizBrain(question_bank)
-quiz.next_question()
 
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You have completed the quiz")
+print(f"Your final score was {quiz.score}/{quiz.question_number}")
 
 
 
--- day-17-Constructing-Class/quiz_brain.py
+++ day-17-Constructing-Class/quiz_brain.py
@@ -2,7 +2,36 @@ class QuizBrain:
     def __init__(self,q_list):
         self.question_number=0
         self.question_list=q_list
+        self.score=0
+
+
 
     def next_question(self):
         current_question=self.question_list[self.question_number]
-        input(f"Q.N.{self.question_number +1}:{current_question.text } (True/False)")
+        self.question_number +=1
+        user_answer=input(f"Q.N.{self.question_number}:{current_question.text } (True/False)")
+        self.check_answer(user_answer,current_question.answer)
+
+    def still_has_questions(self):
+
+        return len(self.question_list) > self.question_number
+
+        # if len(self.question_list) > self.question_number :
+        #     return True
+        # else:
+        #     return False
+
+    def check_answer(self,user_answer,  correct_answer):
+        if user_answer.lower()==correct_answer.lower():
+            self.score +=1
+            print(f"You got it right. Your score is {self.score}/{self.question_number}")
+        else:
+            print(f"Oops! Thats's wrong . Your score is {self.score}/{self.question_number}")
+
+        print(f'The correct answer is {correct_answer}')
+        print("\n")
+
+
+
+
+
