c41502c11d99b634e056c869e381f90343f34269
Shiva Dhakal <theskdhakal@gmail.com>
2025-01-02 10:01:33 +1100

    create a seperate snake class to move the snake

--- day-20-snake-game/main.py
+++ day-20-snake-game/main.py
@@ -1,4 +1,5 @@
-from turtle import Turtle, Screen
+from turtle import  Screen
+from snake import Snake
 import time
 
 
@@ -8,19 +9,7 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-segments=[]
-
-x=0
-y=0
-for _ in range(3):
-    new_segment=Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(x,y)
-    segments.append(new_segment)
-    x-=20
-
-
+snake=Snake()
 
 game_is_on=True
 
@@ -28,22 +17,7 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    for seg_num in range(len(segments)-1,0,-1):
-        new_x=segments[seg_num-1].xcor()
-        new_y=segments[seg_num-1].ycor()
-        segments[seg_num].goto(new_x,new_y)
-
-    segments[0].fd(20)
-
-
-
-
-
-
-
-
-
-
+    snake.move()
 
 
 
---
+++ day-20-snake-game/snake.py
@@ -0,0 +1,29 @@
+from turtle import Turtle
+
+MOVE_DISTANCE=20
+
+class Snake:
+    def __init__(self):
+        self.segments=[]
+        self.create_snake()
+
+
+    def create_snake(self):
+        x = 0
+        y = 0
+        for _ in range(3):
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(x, y)
+            self.segments.append(new_segment)
+            x -= 20
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+            self.segments[seg_num].goto(new_x, new_y)
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].fd(MOVE_DISTANCE)
