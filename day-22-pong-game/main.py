d3593c93c901f07408aa4994688cc3dd496e548e
Shiva Dhakal <theskdhakal@gmail.com>
2025-01-04 10:02:47 +1100

    created a ball and paddle in png game

---
+++ day-22-pong-game/main.py
@@ -0,0 +1,49 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+import time
+
+
+screen=Screen()
+screen.setup(height=600,width=800)
+screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle=Paddle((350,0))
+l_paddle=Paddle((-350,0))
+ball=Ball()
+
+screen.listen()
+screen.onkey(r_paddle.move_up,"Up")
+screen.onkey(r_paddle.move_down,"Down")
+screen.onkey(l_paddle.move_up,"w")
+screen.onkey(l_paddle.move_down,"s")
+
+
+
+is_game_on=True
+
+while is_game_on:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
+
+    #detect collision with top and bottom walls
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() >320 or ball.distance(l_paddle) <50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # detect the paddle missing the ball
+    if ball.xcor() > 380:
+        ball.ball_reset()
+
+    if ball.xcor() < -380:
+       ball.ball_reset()
+
+
+
+screen.exitonclick()
---
+++ day-22-pong-game/paddle.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+
+
+
+
+
+class Paddle(Turtle):
+    def __init__(self,position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.penup()
+        self.speed("fastest")
+        self.shapesize(stretch_len=1,stretch_wid=5)
+        self.goto(position)
+
+
+
+    def move_up(self):
+        self.goto(self.xcor(),self.ycor() + 20)
+
+    def move_down(self):
+        self.goto(self.xcor(),self.ycor() - 20)
