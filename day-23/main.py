9e719fe3234522868bda041d3c5e1f09b7597838
Shiva Dhakal <theskdhakal@gmail.com>
2025-01-05 11:09:57 +1100

    car list created and deployed

--- day-23/main.py
+++ day-23/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 import time
 from player import Player
+from car_manager import CarManager
 
 
 
@@ -11,13 +12,13 @@ screen.setup(width=600,height=600)
 screen.tracer(0)
 screen.bgcolor("black")
 
-
+car_manager=CarManager()
 
 player=Player()
 
 screen.listen()
 
-screen.onkey(player.up,"Up")
+screen.onkeypress(player.up,"Up")
 
 
 game_is_on=True
@@ -26,6 +27,18 @@ while game_is_on:
     time.sleep(0.1)
     screen.update()
 
+    car_manager.create_car()
+    car_manager.move_cars()
+
+
+    #detect collision with car:
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on=False
+
+    #detect a successful crossing:
+    if player.is_at_finish_line():
+        player.go_to_start()
 
 
 
--- day-23/player.py
+++ day-23/player.py
@@ -10,10 +10,19 @@ class Player(Turtle):
         super().__init__()
         self.shape("turtle")
         self.penup()
-        self.goto(STARTING_POSITION)
+        self.go_to_start()
         self.setheading(90)
         self.color("white")
 
 
     def up(self):
         self.fd(MOVE_DISTANCE)
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
+
+    def is_at_finish_line(self):
+        if self.ycor() >FINISH_LINE_Y:
+            return True
+        else:
+            return False
