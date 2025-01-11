c1e85c06ffbf1c53c350e13a04b6965330aadb9a
Shiva Dhakal <theskdhakal@gmail.com>
2025-01-11 13:31:27 +1100

    working with csv file

--- day-25/main.py
+++ day-25/main.py
@@ -20,5 +20,19 @@
 import pandas
 
 data=pandas.read_csv("weather_data.csv")
-print(type(data))
+# print(type(data))
 # print(data["temp"])
+#
+# data_dict=data.to_dict()
+# print(data_dict)
+#
+# temp_list=data["temp"].to_list()
+# print(temp_list)
+#
+# average=sum(temp_list)/len(temp_list)
+# print(f" Average temperature is : {average}")
+#
+# print(data["temp"].mean())
+
+print(data['temp'].max())
+
